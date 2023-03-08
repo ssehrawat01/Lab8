907abf9b0d7893f79143d073affa48a489c7fdb1
ssehrawat01 <ssehrawa@ualberta.ca>
2023-03-07 18:35:56 -0700

    GREEN: new implementation of count cities

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -37,12 +37,12 @@ public class CustomListTest {
     }
 
     @Test
-    void testCountCities() throws Exception {
+    void testCountCities(){
         CustomList cityList = new CustomList(null, citylist);
         City city1 = new City("Mumbai", "Maharashtra");
-        cityList.add(city1);
+        cityList.addCity(city1);
         City city2 = new City("Jaipur", "Rajasthan");
-        cityList.add(city2);
-        assertEquals(3, cityList.countCities());
+        cityList.addCity(city2);
+        assertEquals(2, cityList.countCities());
     }
 }
